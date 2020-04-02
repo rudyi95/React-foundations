659ee6cd0de98ae79c138627e152922d825332e4
rudyi95 <rud11@meta.ua>
2020-04-02 14:09:26 +0300

    first react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Post from "./components/post";
+import './components/post/style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Post
+    name='Anakin Skywalker'
+    photo='https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg'
+    nickname="@dart_vader"
+    content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+    image="https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale"
+    date="26 февр."
+    icon="C:\Users\Lenovo\CURSOR_HomeWork_Rudyi\HomeWork-015\my-app\src\components\post\img\icon.png"
+    comments="486"
+    share="146"
+    like="887"
+    />
     </div>
   );
 }
