abb9b3480c0343f999d6c07241e511630889bbc2
rudyi95 <rud11@meta.ua>
2020-04-02 14:10:52 +0300

    Updates

---
+++ precache-manifest.4cc69ae464c2473ac687102c8f9ca68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6088e7ecdf431d22075d4c855d00fa12",
+    "url": "/React-foundations/index.html"
+  },
+  {
+    "revision": "50fefa1fb64678f4442f",
+    "url": "/React-foundations/static/css/main.8218cf89.chunk.css"
+  },
+  {
+    "revision": "8be4ae44a01160061539",
+    "url": "/React-foundations/static/js/2.7f8c75ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-foundations/static/js/2.7f8c75ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50fefa1fb64678f4442f",
+    "url": "/React-foundations/static/js/main.5ded2ea2.chunk.js"
+  },
+  {
+    "revision": "a0daebb6ece3b2e09ccb",
+    "url": "/React-foundations/static/js/runtime-main.35dec339.js"
+  },
+  {
+    "revision": "c40ac7d9bacbabbd3ef9a830ab2de766",
+    "url": "/React-foundations/static/media/down-arrow.c40ac7d9.svg"
+  }
+]);
